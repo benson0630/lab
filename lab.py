813b39eda99b961b451ca8dc144840312baab644
benson <benson910630@gmail.com>
2023-01-13 00:17:46 +0800

    finish

--- lab.py
+++ lab.py
@@ -1,17 +1,17 @@
 number = {0:'零',1:'壹',2:'貳',3:'參',4:'肆',5:'伍',6:'陸',7:'柒',8:'捌',9:'玖',}
-leng = {0:'',1:'拾',2:'佰',3:'仟'}
-b = []
-ans = input('請輸入1~999999999的數字')
+leng = {0:'',1:'拾',2:'佰',3:'仟',4:'萬',5:'拾',6:'佰',7:'仟',8:'億'}
+ans = []
+money = input('請輸入1~999999999的數字')
 y = 1
-if int(ans) >=1 and int(ans) <=999999999:
-    for i in range(len(ans)):
-        a = number[int(ans[i])]
-        b.append(a)
-        b.append(leng[len(ans)-y])
+if int(money) >=1 and int(money) <=999999999:
+    for i in range(len(money)):
+        a = number[int(money[i])]
+        ans.append(a)
+        ans.append(leng[len(money)-y])
         y = y+1
-    b.append('元整')
-    for i in range(len(b)):
-        print (b[i],end='')
+    ans.append('元整')
+    for i in range(len(ans)):
+        print (ans[i],end='')
 else:
     print('請輸入1~999999999的數字')
  
