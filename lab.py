e45e5556e4a00d914ad68656ec417cc990c05e06
benson <benson910630@gmail.com>
2023-01-13 00:49:51 +0800

    優化

--- lab.py
+++ lab.py
@@ -3,7 +3,11 @@ leng = {0:'',1:'拾',2:'佰',3:'仟',4:'萬',5:'拾',6:'佰',7:'仟',8:'億'}
 ans = []
 money = input('請輸入1~999999999的數字')
 y = 1
-if int(money) >=1 and int(money) <=999999999:
+if int(money) < 1 or int(money) > 999999999:
+    print('請輸入1~999999999的數字')
+elif int(money[:1]) == 0:
+    print('第一位數字請別輸入0')
+else:
     for i in range(len(money)):
         a = number[int(money[i])]
         ans.append(a)
@@ -11,8 +15,5 @@ if int(money) >=1 and int(money) <=999999999:
         y = y+1
     ans.append('元整')
     for i in range(len(ans)):
-        print (ans[i],end='')
-else:
-    print('請輸入1~999999999的數字')
- 
+        print (ans[i],end='') 
     
