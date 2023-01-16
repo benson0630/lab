e34b40c5c287c1420c7b4b80c6c013a3b2d3b5a7
benson <benson910630@gmail.com>
2023-01-16 23:32:11 +0800

    函式只處理字串

--- lab.py
+++ lab.py
@@ -1,4 +1,9 @@
-def digitalTransfer(m,y):   
+def digitalTransfer(m):   
+    number = {0:'零',1:'壹',2:'貳',3:'參',4:'肆',5:'伍',6:'陸',7:'柒',8:'捌',9:'玖',}
+    leng = {0:'',1:'拾',2:'佰',3:'仟',4:'萬',5:'拾',6:'佰',7:'仟',8:'億'}
+    ans = []
+    output = ''
+    y = 1
     if int(m) < 1 or int(m) > 999999999:
         return ('請輸入1~999999999的數字')
     elif int(m[:1]) == 0:
@@ -10,16 +15,10 @@ def digitalTransfer(m,y):
             ans.append(leng[len(m)-y])
             y = y+1
         ans.append('元整')
-        return 1
+        for i in range(len(ans)):
+            output += ans[i] 
+        return output
 
-number = {0:'零',1:'壹',2:'貳',3:'參',4:'肆',5:'伍',6:'陸',7:'柒',8:'捌',9:'玖',}
-leng = {0:'',1:'拾',2:'佰',3:'仟',4:'萬',5:'拾',6:'佰',7:'仟',8:'億'}
-ans = []
 money = input('請輸入1~999999999的數字')
-y = 1
-Transfer = digitalTransfer(money,y)
-if Transfer == 1:
-    for i in range(len(ans)):
-        print (ans[i],end='')
-else:
-    print(Transfer)
+Transfer = digitalTransfer(money)
+print(Transfer)
